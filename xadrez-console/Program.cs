d86899930fbcd9ee0326820a86bf5b214a1ab009
Moises Silva <moises100hp@gmail.com>
2021-02-24 20:56:07 -0300

    Adicionar arquivos de projeto.

---
+++ xadrez-console/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using tabuleiro;
+using Xadrez;
+
+namespace xadrez_console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //PosicaoXadrez pos = new PosicaoXadrez('c', 7);
+            //Console.WriteLine(pos);
+
+            //Console.WriteLine(pos.toPosicao());
+
+            try
+            {
+                PartidaDeXadrez partida = new PartidaDeXadrez();
+
+                while (!partida.terminada)
+                {
+                    Console.Clear();
+                    Tela.imprimirTabuleiro(partida.tab);
+
+                    Console.WriteLine();
+                    Console.Write("Origem: ");
+                    Posicao origem = Tela.lerPosicaoXadrez().toPosicao();
+                    Console.Write("Destino: ");
+                    Posicao destino = Tela.lerPosicaoXadrez().toPosicao();
+
+                    partida.executaMovimento(origem, destino);
+                }
+
+                Tela.imprimirTabuleiro(partida.tab);
+            }
+            catch (Exception e)
+            {
+
+                Console.WriteLine(e.Message);
+            }
+
+
+            Console.ReadLine();
+        }
+    }
+}
